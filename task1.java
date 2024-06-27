c17c19b50293cde6a7026b35e62ae6488ccc6506
nitinmeel <121345090+nitinmeel@users.noreply.github.com>
2024-06-27 13:01:36 +0530

    Create task1.java

---
+++ task1.java
@@ -0,0 +1,50 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessTheNumber {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int lowerBound = 1;
+        int upperBound = 100;
+        int maxAttempts = 10;
+        int score = 0;
+
+        String playAgain = "y";
+        while (playAgain.equalsIgnoreCase("y")) {
+            int numberToGuess = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+            int attempts = 0;
+            boolean guessedCorrectly = false;
+
+            System.out.println("Guess the number between " + lowerBound + " and " + upperBound + ". You have " + maxAttempts + " attempts.");
+
+            while (attempts < maxAttempts && !guessedCorrectly) {
+                System.out.print("Enter your guess: ");
+                int guess = scanner.nextInt();
+                attempts++;
+
+                if (guess < numberToGuess) {
+                    System.out.println("Too low!");
+                } else if (guess > numberToGuess) {
+                    System.out.println("Too high!");
+                } else {
+                    System.out.println("Congratulations! You guessed the correct number!");
+                    guessedCorrectly = true;
+                    score++;
+                }
+            }
+
+            if (!guessedCorrectly) {
+                System.out.println("Sorry, you've run out of attempts. The correct number was " + numberToGuess + ".");
+            }
+
+            System.out.println("Your current score: " + score);
+            System.out.print("Do you want to play again? (y/n): ");
+            playAgain = scanner.next();
+        }
+
+        System.out.println("Thanks for playing! Final score: " + score);
+        scanner.close();
+    }
+}
